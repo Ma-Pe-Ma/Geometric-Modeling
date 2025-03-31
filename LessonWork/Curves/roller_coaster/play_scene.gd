db4cfece2b3b414454ae5331825a14880499a79e
Ma-Pe-Ma <margitai.peter@gmail.com>
2025-03-31 21:09:49 +0200

    Adding tube surface generation

--- LessonWork/Curves/roller_coaster/play_scene.gd
+++ LessonWork/Curves/roller_coaster/play_scene.gd
@@ -5,6 +5,11 @@ class_name PlayScene extends Node
 @export var triad : Node3D;
 @export var point_picker : PointPicker;
 
+@export_range(50, 200, 10) var tube_radius : float = 100.0
+@export_range(3, 32, 1.0) var circumference_division : int = 16
+
+var coaster_mesh : MeshInstance3D = MeshInstance3D.new()
+
 func _ready():
 	play_control.radius_slider.value_changed.connect(self.update_camera)
 	play_control.angle1_slider.value_changed.connect(self.update_camera)
@@ -38,7 +43,11 @@ func _ready():
 			play_control.radius_slider.editable = true
 			play_control.angle1_slider.editable = true
 			play_control.angle2_slider.editable = true
+			
+			triad.basis = point_picker.get_basis_list()[play_control.step_slider.value]
+			triad.position = point_picker.get_point_list()[play_control.step_slider.value]
 			triad.show()
+
 			update_camera(0.0)
 	)
 	
@@ -58,6 +67,75 @@ func update_camera(value : float):
 
 func activate_camera():
 	camera3D.make_current()
+	if not play_control.attach_camera.button_pressed and point_picker.get_point_list().size() > 0:
+		triad.basis = point_picker.get_basis_list()[play_control.step_slider.value]
+		triad.position = point_picker.get_point_list()[play_control.step_slider.value]
 
 func set_max_division_size(max_step_size : int):
 	play_control.step_slider.max_value = max_step_size
+
+# manually build up sweeped circle along the curve path
+func generate_coaster_mesh(points : Array[Vector3], basis_list : Array[Basis]):		
+	var tube_mesh_instance : MeshInstance3D = MeshInstance3D.new()
+	var tube_immediate_mesh : ArrayMesh = ArrayMesh.new()
+	var tube_material : StandardMaterial3D  = StandardMaterial3D.new()
+	
+	tube_mesh_instance.mesh = tube_immediate_mesh
+	tube_mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	var surface_array = []
+	surface_array.resize(Mesh.ARRAY_MAX)
+	
+	var vertices : PackedVector3Array = []
+	var indices : PackedInt32Array = []
+	
+	var angle_step = 2 * PI / circumference_division
+
+	for index in range(2, points.size()):
+		for radius_i in range(0, circumference_division):
+			var angle : float = angle_step * radius_i			
+			
+			var x_comp = tube_radius * cos(angle) * basis_list[index].y
+			var y_comp = tube_radius * sin(angle) * basis_list[index].z
+			
+			var edge_point : Vector3 = points[index] + x_comp + y_comp
+			vertices.append(edge_point)
+	
+	for i in range(2, points.size() - 1):
+		for j in range(circumference_division - 1):
+			# skip every second square for squared pattern
+			if (j % 2 == 0 and i %2 == 0) or (j % 2 == 1 and i %2 == 1):
+				continue
+			
+			indices.append((i - 2) * circumference_division + j)
+			indices.append((i - 2) * circumference_division + j + 1)
+			indices.append((i - 2 + 1) * circumference_division + j)
+			
+			indices.append((i - 2) * circumference_division + j + 1)
+			indices.append((i - 2 + 1) * circumference_division + j + 1)
+			indices.append((i - 2 + 1) * circumference_division + j)
+		
+		# skip last square for squared pattern
+		if i % 2 == 0:
+			continue
+			
+		indices.append((i - 2) * circumference_division + circumference_division - 1)
+		indices.append((i - 2) * circumference_division)
+		indices.append((i - 2 + 1) * circumference_division + circumference_division - 1)
+		
+		indices.append((i - 2) * circumference_division)
+		indices.append((i - 2 + 1) * circumference_division)
+		indices.append((i - 2 + 1) * circumference_division + circumference_division - 1)
+	
+	surface_array[Mesh.ARRAY_VERTEX] = vertices
+	surface_array[Mesh.ARRAY_INDEX] = indices
+	
+	tube_immediate_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	tube_material.albedo_color = Color.BLUE_VIOLET
+	tube_material.cull_mode = BaseMaterial3D.CULL_DISABLED
+	tube_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	tube_immediate_mesh.surface_set_material(0, tube_material)	
+	
+	self.coaster_mesh.queue_free()
+	self.coaster_mesh = tube_mesh_instance
+	add_child(tube_mesh_instance)
--- LessonWork/Curves/roller_coaster/scene_handler.gd
+++ LessonWork/Curves/roller_coaster/scene_handler.gd
@@ -11,21 +11,23 @@ func _ready() -> void:
 				play_scene.activate_camera()
 				play_scene.set_max_division_size(point_picker.picker_gui.division_slider.value)
 				set_node_state(point_picker, false)
-				set_marker_scale(0.5)	
+				play_scene.generate_coaster_mesh(point_picker.get_point_list(), point_picker.get_basis_list())
+				
+				for control_point_marker in point_picker.get_children() as Array[Node3D]:
+					control_point_marker.hide()
 			1:
+				for control_point_marker in point_picker.get_children() as Array[Node3D]:
+					control_point_marker.show()
+				
+				play_scene.coaster_mesh.hide()
 				set_node_state(point_picker, true)
 				point_picker.activate_camera()
-				set_marker_scale(1.0)
 	)	
 
 	tab_container.tab_changed.emit(0)
 
-func set_node_state(node : Node, state : bool):
+func set_node_state(node : Node, state : bool) -> void:
 	node.set_process(state)
 	node.set_physics_process(state)
 	node.set_process_unhandled_input(state)
 	node.set_process_input(state)
-
-func set_marker_scale(scale : float = 1.0) -> void:
-	for child in point_picker.get_children() as Array[Node3D]:
-		child.scale = Vector3(scale, scale, scale)
